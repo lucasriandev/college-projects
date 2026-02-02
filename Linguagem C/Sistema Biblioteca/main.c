2e3c6fee0d337286a3ffeb844300cbc0980d8747
Lucas <lucasrian.dev@gmail.com>
2026-02-02 13:47:43 -0300

    Adicionando comentarios

--- Linguagem C/Sistema Biblioteca/main.c
+++ Linguagem C/Sistema Biblioteca/main.c
@@ -23,12 +23,23 @@ int main(){
         printf("Digite o nome do territorio: ");
         scanf(" %29[^\n]", mapa[i].nome);
 
+        //% = Início do comando.
+        //29 = Tamanho máximo (segurança).
+        //[...] = Conjunto de caracteres aceitos.
+        //^ = NÃO.
+        //\n = ENTER (Nova linha).
+        // tradução: "Leia um texto de no máximo 29 letras contendo qualquer coisa QUE NÃO SEJA um Enter."
+
+
         printf("Digite a cor do exercito: ");
         scanf(" %9s", mapa[i].cor);
+        //%9s é s de string
 
         printf("Quantidade de tropas: ");
         scanf("%d", &mapa[i].tropas);
-
+        //%d vem Decimal
+        //& é o endereço, "Leia um número e entregue na casa onde mora a variável tropas"
+       
         printf("----------------------\n");
         }
 
