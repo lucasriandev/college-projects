68c75099c8fb5ed39f759d0fb4b347780e2ad9aa
Lucas <lucasrian.dev@gmail.com>
2026-02-02 13:32:41 -0300

    Finalizado primeiro projeto

--- Linguagem C/Sistema Biblioteca/main.c
+++ Linguagem C/Sistema Biblioteca/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
+//Definição da struct Territorio para agrupar os dados
 struct Territorio
 {
     char nome[30];
@@ -11,5 +12,36 @@ struct Territorio
 int main(){
 
     struct Territorio mapa[5];    
-    printf("SISTEMA");
+
+    int i;
+
+    printf("=== SISTEMA DE CADASTRO DE TERRITORIO ===\n\n");
+
+    //loop para ler os dados do territorio
+    for(i = 0; i < 5; i++){
+        printf("Cadastro do territorio %d:\n", i + 1);
+        printf("Digite o nome do territorio: ");
+        scanf(" %29[^\n]", mapa[i].nome);
+
+        printf("Digite a cor do exercito: ");
+        scanf(" %9s", mapa[i].cor);
+
+        printf("Quantidade de tropas: ");
+        scanf("%d", &mapa[i].tropas);
+
+        printf("----------------------\n");
+        }
+
+        printf("\n\n=== RESUMO DOS TERRITORIOS ===\n");
+        for(i = 0; i < 5; i++){
+            printf("Territorio %d: %s | Cor: %s | Tropas: %d\n",
+            i+1,
+            mapa[i].nome,
+            mapa[i].cor,
+            mapa[i].tropas
+            );
+        }
+        return 0;
 };
+
+
