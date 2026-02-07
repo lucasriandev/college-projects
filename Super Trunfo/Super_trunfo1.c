916ddc0cdc62cc1f31b4fc82357993b9d338f9a7
Lucas <lucasrian.dev@gmail.com>
2026-02-06 22:08:19 -0300

    Refazendo logica de acordo com os requisitos da faculdade

--- Super Trunfo/Super_trunfo1.c
+++ Super Trunfo/Super_trunfo1.c
@@ -1,46 +1,46 @@
 //Projeto novo da faculdade
 
-#include <string.h>
 #include <stdio.h>
 
-
-struct Carta
-{
-    char estado;
-    char codigo[4];
-    char cidade[50];
-    char populacao;
-    char area;
-    char pib;
-    char pontosTuristicos;
-};
-
-
 int main(){
-    struct Carta carta1;
-    struct Carta carta2;
+    char estado1;
+    char codigo1[4];
+    char nomeCidade1[50];
+    char populacao1;
+    char area1;
+    char pib1;
+    char pontosTuristicos1;
 
-    printf("Cadastro da Carta 1\n");
+    char estado2;
+    char codigo2[4];
+    char nomeCidade[50];
+    char populacao2;
+    char area2;
+    char pib2;
+    char pontosTuristicos2;
 
-    printf("Estado: ");
-    scanf(" %c", &carta1.estado);
-
-    printf("Codigo: ");
-    scanf("%s", carta1.codigo);
-
-    printf("Nome da cidade: ");
-    scanf(" %[^\n]", carta1.cidade);
-
-    printf("Populacao: ");
-    scanf("%d", &carta1.populacao);
-
-    printf("Area: ");
-    scanf("%f", &carta1.area);
-
-    printf("PIB: ");
-    scanf("%f", &carta1.pib);
-
-    printf("Pontos turisticos: ");
-    scanf("%d", &carta1.pontosTuristicos);
-
+    printf("=== CADASTRO DA CARTA 1 ===\n");
+    
+    printf("Digite o Estado (uma letra de A-H): ");
+    // O espaço antes do %c serve para limpar o buffer do teclado
+    scanf(" %c", &estado1); 
+    
+    printf("Digite o Codigo da Carta (ex: A01): ");
+    scanf("%s", codigo1); // String não precisa do &
+    
+    printf("Digite o Nome da Cidade: ");
+    // O comando " %[^\n]" lê textos com espaços (ex: Sao Paulo) até o usuário apertar Enter
+    scanf(" %[^\n]", nomeCidade1);
+    
+    printf("Digite a Populacao: ");
+    scanf("%d", &populacao1);
+    
+    printf("Digite a Area (em km2): ");
+    scanf("%f", &area1);
+    
+    printf("Digite o PIB (em bilhoes): ");
+    scanf("%f", &pib1);
+    
+    printf("Digite o Numero de Pontos Turisticos: ");
+    scanf("%d", &pontosTuristicos1);
 }
