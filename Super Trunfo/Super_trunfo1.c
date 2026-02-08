0ea5fcdbfcd8bb16ff22a7e929074c30a8699086
Lucas <lucasrian.dev@gmail.com>
2026-02-07 21:18:48 -0300

    Corrigindo Erro nas variaveis do super trunfo 1

--- Super Trunfo/Super_trunfo1.c
+++ Super Trunfo/Super_trunfo1.c
@@ -6,18 +6,18 @@ int main(){
     char estado1;
     char codigo1[4];
     char nomeCidade1[50];
-    char populacao1;
-    char area1;
-    char pib1;
-    char pontosTuristicos1;
+    int populacao1;
+    float area1;
+    float pib1;
+    int pontosTuristicos1;
 
     char estado2;
     char codigo2[4];
     char nomeCidade2[50];
-    char populacao2;
-    char area2;
-    char pib2;
-    char pontosTuristicos2;
+    int populacao2;
+    float area2;
+    float pib2;
+    int pontosTuristicos2;
 
     printf("=== CADASTRO DA CARTA 1 ===\n");
     
